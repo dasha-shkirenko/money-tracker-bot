ce8d57fa697c1d4c6dc93e0dea121458c4e68095
dasha-shkirenko <daria.shkirenko@gmail.com>
2018-07-06 18:14:10 +0300

    adding calendar and /statistic command

--- bot.py
+++ bot.py
@@ -4,8 +4,10 @@ import os
 import telebot
 from telebot import types
 import gsheet
+from telegramcalendar import create_calendar
+import datetime
 
-#bot = telebot.TeleBot(config.token)
+# bot = telebot.TeleBot(config.token)
 bot = telebot.TeleBot(os.environ['telegram_token'])
 auth_users = [445219449, 394294378]
 
@@ -18,6 +20,78 @@ cost_lst = [
 ]
 
 shared_memory = {}
+current_shown_dates = {}
+
+@bot.message_handler(commands=['statistic'])
+def calendar(message):
+    now = datetime.datetime.now()  # Current date
+    chat_id = message.chat.id
+    date = (now.year, now.month)
+    current_shown_dates[chat_id] = date  # Saving the current date in a dict
+    markup = create_calendar(now.year, now.month)
+    bot.send_message(message.chat.id, "Please, choose a date", reply_markup=markup)
+
+    shared_memory['chat_id'] = message.chat.id
+    shared_memory['user_first_name'] = message.from_user.first_name
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'next-month')
+def next_month(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        year,month = saved_date
+        month+=1
+        if month>12:
+            month=1
+            year+=1
+        date = (year,month)
+        current_shown_dates[chat_id] = date
+        markup= create_calendar(year,month)
+        bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
+        bot.answer_callback_query(call.id, text="")
+    else:
+        #Do something to inform of the error
+        pass
+
+@bot.callback_query_handler(func=lambda call: call.data == 'previous-month')
+def previous_month(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        year,month = saved_date
+        month-=1
+        if month<1:
+            month=12
+            year-=1
+        date = (year,month)
+        current_shown_dates[chat_id] = date
+        markup= create_calendar(year,month)
+        bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
+        bot.answer_callback_query(call.id, text="")
+    else:
+        pass
+
+
+@bot.callback_query_handler(func=lambda call: call.data[0:13] == 'calendar-day-')
+def get_day(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        day = call.data[13:]
+        choosen_date = datetime.date(int(saved_date[0]),int(saved_date[1]),int(day))
+        # bot.send_message(chat_id, str(choosen_date))
+        bot.answer_callback_query(call.id, text="")
+        shared_memory['choosen_date'] = call.message.text
+
+        print(choosen_date)
+        cost_amount = gsheet.find_data(shared_memory['user_first_name'], str(choosen_date))
+        bot.send_message(chat_id, 'Date: {}, amount of costs: {}'.format(choosen_date, cost_amount))
+
+    else:
+        pass
+
+
 
 
 @bot.message_handler(commands=['start'])
@@ -42,7 +116,7 @@ def func(message):
         try:
             int(message.text)
             shared_memory['cost_amount'] = message.text
-            bot.send_message(message.chat.id, 'Cost type is: {}, amount is {}'.format(
+            bot.send_message(message.chat.id, 'Cost type is {}, amount is {}'.format(
                 shared_memory['cost_type'],
                 shared_memory['cost_amount'])
                              )
@@ -51,6 +125,7 @@ def func(message):
             bot.send_message(message.chat.id, 'Incorrect input {}'.format(v))
     else:
         bot.send_message(message.chat.id, 'Access denied')
+
 
 
 bot.polling()
--- gsheet.py
+++ gsheet.py
@@ -11,7 +11,7 @@ credentials.private_key_id = os.environ['private_key_id']
 
 open_file = gspread.authorize(credentials).open('MoneyTrackerBot')
 
-record_date = str(datetime.datetime.now().strftime("%m-%d-%Y"))
+record_date = str(datetime.datetime.now().strftime("%Y-%m-%d"))
 record_time = str(datetime.datetime.now().strftime("%H:%M"))
 
 
@@ -29,3 +29,18 @@ def add_to_sheet(user_name, cost_type, cost_amount):
         open_file.worksheet(user_name).append_row(
             [record_date, record_time, cost_type, cost_amount]
         )
+def find_data(user_name, choosen_date):
+    rd = open_file.worksheet(user_name).findall(choosen_date)
+    rows = []
+
+    for i in rd:
+        rows.append(open_file.worksheet(user_name).row_values(i.row))
+
+    total_cost_amount = sum([int(row[3]) for row in rows])
+    return total_cost_amount
+
+
+
+
+
+
